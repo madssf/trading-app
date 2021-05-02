94be85947b114a0d4d069d69fff74188194bb475
madssf <mads.steenfeldt.foss@gmail.com>
2021-05-02 15:09:58 +0200

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trading-app
+
+source /Users/mads/.pyenv/versions/trading-app/bin/activate
---
+++ dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.sidebar.title('options')
+st.header('dashboard')
+st.write('pf_data')
+st.write('model_inputs')
+st.write('staked')
+st.write('trade_log')
+st.write('order_log')
